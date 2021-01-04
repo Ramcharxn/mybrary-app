120796e1c344d85d9db3f59c4d044e3b3f5b26f8
Ramcharxn <ramcharxn@gmail.com>
2021-01-04 16:33:56 +0530

    inital routes

--- models/article.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const articleSchema = new mongoose.Schema({
-    title:{
-        type:String,
-    },
-})
-
-module.exports = mongoose.model('Article'.articleSchema)
